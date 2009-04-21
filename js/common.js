bfe4a84b8aa2d3d4bc96968f623c40b022b54264
section31 <section31@gmail.com>
2009-04-20 21:57:31 -0500

    First Commit

---
+++ js/common.js
@@ -0,0 +1,31 @@
+var Site = {
+
+	// this vars should be set in <head> server-side
+	config : {
+		base_url : '',
+		site_url : ''
+	},
+	
+	// this method is called on every page
+	start : function() {
+
+		// On Dom Ready
+		jQuery(function($) {
+			
+		});
+		
+		// Load Immediately
+		(function($) {
+		
+		})(jQuery);
+
+
+		// On Window Load
+		jQuery(window).load(function ($) {
+
+		});
+
+	}
+};
+
+Site.start();
